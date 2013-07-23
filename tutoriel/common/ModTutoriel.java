522d57cbe78d87a0f15d3fe5daca608be3d8684e
robin <mc-robin4002@gmx.fr>
2013-07-24 00:31:08 +0200

    Ajout d'un fichier de configuration

--- tutoriel/common/ModTutoriel.java
+++ tutoriel/common/ModTutoriel.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.EnumHelper;
 import net.minecraftforge.common.MinecraftForge;
 import tutoriel.client.EventSoundTutorial;
@@ -33,12 +34,40 @@ public class ModTutoriel
 	public static Block BlockTutorial, TutorialMetadata;
 	public static Item ItemTutorial, ItemWithMetadata, TutorialHelmet, TutorialChestPlate, TutorialLeggings, TutorialBoots;
 	
+	public static int BlockTutorialID, TutorialMetadataID, ItemTutorialID, ItemWithMetadataID, TutorialHelmetID,
+	TutorialChestPlateID, TutorialLeggingsID, TutorialBootsID;
+	
 	static EnumArmorMaterial TutorialArmor = EnumHelper.addArmorMaterial("Tutorial", 20, new int[]{2, 8, 4, 2}, 15);
 	
 	@EventHandler
 	public void PreInit(FMLPreInitializationEvent event)
 	{
 		//Configuration
+		Configuration cfg = new Configuration(event.getSuggestedConfigurationFile());
+		try
+		{
+			cfg.load();
+			BlockTutorialID = cfg.getBlock("Block Tutoriel", 2000, "this is a comment").getInt();
+			TutorialMetadataID = cfg.getBlock("Block Tutoriel Metadata", 2001).getInt();
+			
+			ItemTutorialID = cfg.getItem("Item Tutoriel", 12000).getInt();
+			ItemWithMetadataID = cfg.getItem("Item With Metadata", 12001).getInt();
+			TutorialHelmetID = cfg.getItem("Tutorial Helmet", 12002).getInt();
+			TutorialChestPlateID = cfg.getItem("Tutorial Chest Plate", 12003).getInt();
+			TutorialLeggingsID = cfg.getItem("Tutorial Leggings", 12004).getInt();
+			TutorialBootsID = cfg.getItem("Tutorial Boots", 12005).getInt();
+		}
+		catch(Exception ex)
+		{
+			event.getModLog().severe("Failed to load configuration");
+		}
+		finally
+		{
+			if(cfg.hasChanged())
+			{
+				cfg.save();
+			}
+		}
 		
 		//Son - sound
 		if(event.getSide().isClient())
@@ -47,20 +76,20 @@ public class ModTutoriel
 		}
 		
 		//Blocks
-		BlockTutorial = new BlockTutorial(2000).setHardness(1.0F).setResistance(5.0F).setStepSound(Block.soundStoneFootstep).setUnlocalizedName("BlockTutorial").func_111022_d("modtutoriel:BlockTutorial");
-		TutorialMetadata = new BlockTutorialMetadata(2001).setHardness(2.0F).setResistance(10.0F).setStepSound(Block.soundStoneFootstep).setUnlocalizedName("TutorialMetadata");
+		BlockTutorial = new BlockTutorial(BlockTutorialID).setHardness(1.0F).setResistance(5.0F).setStepSound(Block.soundStoneFootstep).setUnlocalizedName("BlockTutorial").func_111022_d("modtutoriel:BlockTutorial");
+		TutorialMetadata = new BlockTutorialMetadata(TutorialMetadataID).setHardness(2.0F).setResistance(10.0F).setStepSound(Block.soundStoneFootstep).setUnlocalizedName("TutorialMetadata");
 
 		//Enregistrement des blocs - Blocks registry
 		GameRegistry.registerBlock(BlockTutorial, "BlockTutorial");
 		GameRegistry.registerBlock(TutorialMetadata, ItemBlockTutorialMetadata.class, "TutorialMetadata", "ModTutoriel");
 		
 		//Items
-		ItemTutorial = new ItemTutorial(12000).setUnlocalizedName("ItemTutorial").func_111206_d("modtutoriel:ItemTutorial");
-		ItemWithMetadata = new ItemWithMetadata(12001).setUnlocalizedName("ItemWithMetadata");
-		TutorialHelmet = new ItemTutorialArmor(12002, TutorialArmor, 0,0).setUnlocalizedName("TutorialHelmet").func_111206_d("modtutoriel:HelmetTutorial");
-		TutorialChestPlate = new ItemTutorialArmor(12003, TutorialArmor, 0, 1).setUnlocalizedName("TutorialChestPlate").func_111206_d("modtutoriel:ChestPlateTutorial");
-		TutorialLeggings = new ItemTutorialArmor(12004, TutorialArmor, 0, 2).setUnlocalizedName("TutorialLeggings").func_111206_d("modtutoriel:LeggingsTutorial");
-		TutorialBoots = new ItemTutorialArmor(12005, TutorialArmor, 0, 3).setUnlocalizedName("TutorialBoots").func_111206_d("modtutoriel:BootsTutorial");
+		ItemTutorial = new ItemTutorial(ItemTutorialID).setUnlocalizedName("ItemTutorial").func_111206_d("modtutoriel:ItemTutorial");
+		ItemWithMetadata = new ItemWithMetadata(ItemWithMetadataID).setUnlocalizedName("ItemWithMetadata");
+		TutorialHelmet = new ItemTutorialArmor(TutorialHelmetID, TutorialArmor, 0, 0).setUnlocalizedName("TutorialHelmet").func_111206_d("modtutoriel:HelmetTutorial");
+		TutorialChestPlate = new ItemTutorialArmor(TutorialChestPlateID, TutorialArmor, 0, 1).setUnlocalizedName("TutorialChestPlate").func_111206_d("modtutoriel:ChestPlateTutorial");
+		TutorialLeggings = new ItemTutorialArmor(TutorialLeggingsID, TutorialArmor, 0, 2).setUnlocalizedName("TutorialLeggings").func_111206_d("modtutoriel:LeggingsTutorial");
+		TutorialBoots = new ItemTutorialArmor(TutorialBootsID, TutorialArmor, 0, 3).setUnlocalizedName("TutorialBoots").func_111206_d("modtutoriel:BootsTutorial");
 		
 		//Enregistrement des items - Item registry
 		GameRegistry.registerItem(ItemTutorial, "ItemTutorial", "ModTutoriel");
